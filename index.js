b596b28a2eea9765d28740d9d6337dcdaf02c561
shani <shhhaniburde@gmail.com>
2022-01-03 21:22:24 -0500

    Added bootstrap

--- index.js
+++ index.js
@@ -22,19 +22,22 @@ function getAllFood(){
 function renderImages(foodObj){
     console.log(foodObj)
     let getObj = document.querySelector('#detail-info')
+     getObj.className = "container"
+     getObj.className = "text-center"
     let title = document.createElement('h4')
         title.id = 'post-title'
     let caption = document.createElement('h6')
         caption.id = 'post-caption'
     let url = document.createElement('img')
         url.id = 'post-url'
+        url.className = "rounded mx-auto d-block"
     let likeButton = document.createElement('button')
         likeButton.id = 'post-likeButton'
     let comment = document.createElement('p')
         comment.id = 'post-comment'
 
     title.textContent = foodObj.title
-    caption.textContent = foodObj.caption
+    caption.textContent = foodObj.captions
     url.src = foodObj.url
     likeButton.textContent = "♡"
     //add event listener to turn the heart red
